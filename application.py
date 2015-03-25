e135aa38b5c6617dbb7789032b1c55ec11f115a0
jimjshields <jim.j.shields@gmail.com>
2015-03-25 10:59:37 -0400

    Works well enough for now.

--- application.py
+++ application.py
@@ -26,9 +26,10 @@ class Projects(object):
 
 all_projects = Projects()
 
+all_projects.add_project('tiny', 'Tiny', 'A web framework built in Python.', 'https://github.com/jimjshields/tiny')
 all_projects.add_project('thank_the_academy', 'Thank the Academy', 'Randomly generate an Oscar acceptance speech.', 'http://thanktheacademy.herokuapp.com')
 all_projects.add_project('heatmap', 'Box Office Heatmap', 'Get a heatmap of the weekly box office, from 1999 to now.', 'projects/heatmap')
-all_projects.add_project('tiny', 'Tiny', 'A small web framework built in Python, built as a learning project.', 'https://github.com/jimjshields/tiny')
+all_projects.add_project('speech_analysis', 'Oscar Speech Analysis', 'Interactive visualization of all Oscar speeches since 1966.', 'http://thanktheacademy.herokuapp.com/analysis')
 all_projects.add_project('rt_cli', 'Rotten Tomatoes CLI', 'A command-line interface for Rotten Tomatoes\' API.', 'https://github.com/jimjshields/rotten_tomatoes_CLI')
 all_projects.add_project('best_picture', 'Scraping the Best Picture\'s Budgets', 'A scraper that grabs all of the available budgets of all Best Picture winners from Wikipedia.', 'https://github.com/jimjshields/best_picture')
 all_projects.add_project('myvcs', 'My VCS', 'A simple, not-quite-finished version control system.', 'https://github.com/jimjshields/myvcs')
